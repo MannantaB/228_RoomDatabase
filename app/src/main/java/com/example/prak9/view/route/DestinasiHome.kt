3b4af4547aceb096ea5b2101e0654fbc7605c745
MannantaB <mannanta18@gmail.com>
2025-11-27 12:14:04 +0700

    add Home destination route

---
+++ app/src/main/java/com/example/prak9/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.prak9.view.route
+
+import com.example.prak9.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
