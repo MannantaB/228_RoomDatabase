bf8e5434414661aefafe21b4390b27dd7200d76b
MannantaB <mannanta18@gmail.com>
2025-11-27 11:50:29 +0700

    add RepositoriSiswa.kt

---
+++ app/src/main/java/com/example/prak9/repositori/RepositoriSiswa.kt
@@ -0,0 +1,17 @@
+package com.example.prak9.repositori
+
+import com.example.prak9.room.Siswa
+import com.example.prak9.room.SiswaDao
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+}
+
+class OfflineRepositoriSiswa(
+    private val siswaDao: SiswaDao
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
