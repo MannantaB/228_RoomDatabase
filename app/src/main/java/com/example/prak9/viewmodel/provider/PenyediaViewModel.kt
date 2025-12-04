db39a997ae3566d22becbf422c3c335ca24fc55a
MannantaB <mannanta18@gmail.com>
2025-12-04 13:35:28 +0700

    add initializer for DetailViewModel

--- app/src/main/java/com/example/prak9/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak9/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.prak9.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.prak9.repositori.AplikasiSiswa
+import com.example.prak9.viewmodel.DetailViewModel
+import com.example.prak9.viewmodel.EditViewModel
 import com.example.prak9.viewmodel.EntryViewModel
 import com.example.prak9.viewmodel.HomeViewModel
 
@@ -15,6 +18,9 @@ object PenyediaViewModel {
         }
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(),aplikasiSiswa().container.repositoriSiswa)
         }
     }
 }
